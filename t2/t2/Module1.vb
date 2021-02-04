612f1dd9f4ba6e52c1405b452688d6356f2cba01
雄龙ztz <25386968+xionglongztz@users.noreply.github.com>
2021-02-04 19:10:31 +0800

    Delete t2/t2 directory

--- t2/t2/Module1.vb
+++
@@ -1,4 +0,0 @@
-﻿Module Module1
-    Public Declare Function GetPrivateProfileString Lib “kernel32” Alias “GetPrivateProfileStringA” (ByVal lpApplicationName As String, ByVal lpKeyName As String, ByVal lpDefault As String, ByVal lpReturnedString As String, ByVal nSize As Integer, ByVal lpFileName As String) As Integer '读ini文件
-    Public Declare Function WritePrivateProfileString Lib “kernel32” Alias “WritePrivateProfileStringA” (ByVal lpApplicationName As String, ByVal lpKeyName As String, ByVal lpString As String, ByVal lpFileName As String) As Integer '写ini文件
-End Module
